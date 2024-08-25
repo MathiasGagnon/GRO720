f8f0f56a15e3669c1cdf13da9002cb3a66642000
MathiasGagnon <Mathias.Gagnon@USherbrooke.ca>
2024-08-25 18:02:08 -0400

    Added softmax and CrossEntropyLoss (tests pass)

--- Problématique/dnn_framework/student/losses.py
+++ Problématique/dnn_framework/student/losses.py
@@ -14,7 +14,29 @@ class CrossEntropyLoss(Loss):
         :param target: The target classes (shape: (N,))
         :return A tuple containing the loss and the gradient with respect to the input (loss, input_grad)
         """
-        raise NotImplementedError()
+        x_softmax = softmax(x)
+
+        n_samples = x.shape[0]
+        n_classes = x.shape[1]
+
+        target_samples = np.zeros(x.shape)
+
+        for sample_index in range(n_samples):
+            for class_index in range(n_classes):
+                if target[sample_index] == class_index:
+                    target_samples[sample_index, class_index] = 1
+                else:
+                    target_samples[sample_index, class_index] = 0
+        
+        losses = []
+        for sample_index in range(n_samples):
+            losses.append(-1*np.sum(target_samples[sample_index] * np.log(x_softmax[sample_index])))
+
+        loss = np.mean(losses)
+
+        input_grad = (x_softmax - target_samples) / n_samples
+
+        return loss, input_grad
 
 
 def softmax(x):
@@ -22,7 +44,13 @@ def softmax(x):
     :param x: The input tensor (shape: (N, C))
     :return The softmax of x
     """
-    raise NotImplementedError()
+    softmax_values = []
+    for sample_index in range(x.shape[0]):
+        x_i = x[sample_index]
+        softmax_values.append(np.exp(x_i)/np.sum(np.exp(x_i)))
+
+    softmax_values = np.array(softmax_values)
+    return softmax_values
 
 
 class MeanSquaredErrorLoss(Loss):
