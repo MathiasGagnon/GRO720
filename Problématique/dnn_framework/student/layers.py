f6d392254c15c37474fdc21d710560b8ae4f0665
Y0nyx <72567319+Y0nyx@users.noreply.github.com>
2024-09-06 13:56:50 -0400

    test pass

--- Problématique/dnn_framework/student/layers.py
+++ Problématique/dnn_framework/student/layers.py
@@ -1,7 +1,9 @@
 import numpy as np
 
 from dnn_framework.layer import Layer
+
 epsilon = 1e-6
+
 
 class FullyConnectedLayer(Layer):
     """
@@ -9,9 +11,10 @@ class FullyConnectedLayer(Layer):
     """
 
     def __init__(self, input_count, output_count):
-         self.param = {
-            'w': np.random.randn(output_count, input_count) * np.sqrt(2 / (input_count + output_count)),
-            'b': np.random.randn(output_count) * np.sqrt(2 / output_count)
+        self.param = {
+            "w": np.random.randn(output_count, input_count)
+            * np.sqrt(2 / (input_count + output_count)),
+            "b": np.random.randn(output_count) * np.sqrt(2 / output_count),
         }
 
     def get_parameters(self):
@@ -21,10 +24,13 @@ class FullyConnectedLayer(Layer):
         return {}
 
     def forward(self, x):
-        return np.matmul(x, self.param['w'].T) + self.param['b'], x
+        return np.matmul(x, self.param["w"].T) + self.param["b"], x
 
     def backward(self, output_grad, cache):
-        return np.dot(output_grad, self.param['w']), {'w':np.dot(output_grad.T, cache), 'b':np.sum(output_grad, axis=0)}
+        return np.dot(output_grad, self.param["w"]), {
+            "w": np.dot(output_grad.T, cache),
+            "b": np.sum(output_grad, axis=0),
+        }
 
 
 class BatchNormalization(Layer):
@@ -34,59 +40,89 @@ class BatchNormalization(Layer):
 
     def __init__(self, input_count, alpha=0.1):
         super().__init__()
+        self.epsilon = 1 * 10**-7
         self.alpha = alpha
-        self.buffer = {'global_mean':np.zeros(input_count), 'global_variance': np.zeros(input_count)}
-        self.param = {'gamma': np.ones(input_count), 'beta': np.zeros(input_count)}
+
+        self.parameters = {"gamma": np.ones(input_count), "beta": np.zeros(input_count)}
+        self.buffers = {
+            "global_mean": np.zeros(input_count),
+            "global_variance": np.zeros(input_count),
+        }
+
+        self.first = False
 
     def get_parameters(self):
-        return self.param
+        return self.parameters
 
     def get_buffers(self):
-        return self.buffer
+        return self.buffers
 
     def forward(self, x):
-        if self._is_training:
+        if self.is_training():
             return self._forward_training(x)
         return self._forward_evaluation(x)
 
     def _forward_training(self, x):
         batch_mean = np.mean(x, axis=0)
-        batch_variance = np.var(x, axis=0)
+        batch_var = np.var(x, axis=0)
 
-        if np.all(self.buffer['global_mean'] == 0):
-            self.buffer['global_mean'] = batch_mean
+        self.buffers["global_mean"] = (
+            (1 - self.alpha) * self.buffers["global_mean"] + self.alpha * batch_mean
+            if self.first
+            else batch_mean
+        )
+        self.buffers["global_variance"] = (
+            (1 - self.alpha) * self.buffers["global_variance"] + self.alpha * batch_var
+            if self.first
+            else batch_var
+        )
 
-        if np.all(self.buffer['global_variance'] == 0):
-            self.buffer['global_variance'] = batch_variance
+        self.first = True
 
-        self.buffer['global_mean'] = (1 - self.alpha) * self.buffer['global_mean'] + self.alpha * batch_mean
-        self.buffer['global_variance'] = (1 - self.alpha) * self.buffer['global_variance'] + self.alpha * batch_variance
-
-        return self._forward_evaluation(x)
+        x_hat = (x - batch_mean) / (batch_var + self.epsilon) ** 0.5
+        y = self.parameters["gamma"] * x_hat + self.parameters["beta"]
+        return y, (x, x_hat)
 
     def _forward_evaluation(self, x):
-        y = self.param['gamma'] * self.get_x_hat(x) + self.param['beta']
-        return y, x
+        xhat = (x - self.buffers["global_mean"]) / (
+            self.buffers["global_variance"] + self.epsilon
+        ) ** 0.5
+        return self.parameters["gamma"] * xhat + self.parameters["beta"], (
+            x,
+            xhat,
+            self.buffers["global_mean"],
+            self.buffers["global_variance"],
+        )
 
     def backward(self, output_grad, cache):
-        x = cache
-        M = cache.shape[0]
-        gamma = self.param['gamma']
-        batch_mean = np.mean(x, axis=0)
-        batch_variance = np.var(x, axis=0)
+        grad_dict = {}
+        x, xhat = cache
+        M = x.shape[0]
 
+        grad_xhat = output_grad * self.parameters["gamma"]
+        grad_var = np.sum(
+            grad_xhat
+            * (x - self.buffers["global_mean"])
+            * (-1 / 2)
+            * (self.buffers["global_variance"] + self.epsilon) ** (-3 / 2),
+            axis=0,
+        )
+        grad_mean = -np.sum(
+            grad_xhat / (self.buffers["global_variance"] + self.epsilon) ** (1 / 2),
+            axis=0,
+        )
+        grad_x = (
+            grad_xhat / (self.buffers["global_variance"] + self.epsilon) ** (1 / 2)
+            + 2 / M * grad_var * (x - self.buffers["global_mean"])
+            + 1 / M * grad_mean
+        )
 
-        x_grad = (1.0 / M) * gamma * 1 / np.sqrt(batch_variance + epsilon) * (M * output_grad - np.sum(output_grad, axis=0) \
-                 - (x - batch_mean) * 1 / (batch_variance + epsilon) * np.sum(output_grad * (x - batch_mean), axis=0))
+        grad_gamma = np.sum(output_grad * xhat, axis=0)
+        grad_beta = np.sum(output_grad, axis=0)
 
-        grad_x_hat = output_grad * self.param['gamma']
-        grad_var = np.sum(grad_x_hat * (cache-self.buffer['global_mean']) * -0.5 * (self.buffer['global_variance'] + epsilon)**(-1.5), axis=0)
-        grad_mean = -np.sum(grad_x_hat/np.sqrt(self.buffer['global_variance'] + epsilon), axis=0)
-        grad_x = (grad_x_hat/np.sqrt(self.buffer['global_variance'])) + (2/M) * grad_var * (cache-cache-self.buffer['global_mean']) + 1/M * grad_mean
-        return x_grad , {'gamma': np.sum(output_grad * self.get_x_hat(cache), axis=0), 'beta': np.sum(output_grad, axis=0)}
-    
-    def get_x_hat(self, x):
-        return (x - self.buffer['global_mean']) / np.sqrt(self.buffer['global_variance'] + epsilon)
+        grad_dict["gamma"] = grad_gamma
+        grad_dict["beta"] = grad_beta
+        return grad_x, grad_dict
 
 
 class Sigmoid(Layer):
@@ -101,7 +137,7 @@ class Sigmoid(Layer):
         return {}
 
     def forward(self, x):
-        return 1/(1+ np.exp(-x)), x
+        return 1 / (1 + np.exp(-x)), x
 
     def backward(self, output_grad, cache):
         return self.forward(cache)[0] * (1 - self.forward(cache)[0]) * output_grad, {}
